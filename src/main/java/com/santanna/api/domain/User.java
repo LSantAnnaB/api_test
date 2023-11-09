9b60502128ad65833ba81e564f7f1366a7868213
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-11-09 10:47:30 -0300

    user entity; user dto

---
+++ src/main/java/com/santanna/api/domain/User.java
@@ -0,0 +1,26 @@
+package com.santanna.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_user")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String name;
+
+  @Column(unique = true)
+  private String email;
+  private String password;
+
+}
